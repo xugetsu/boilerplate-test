4ff02a51d3722e32c9ddafd115e7b12ef9ec3462
xugetsu <aliot@xtech.guru>
2021-05-17 22:14:30 +0100

    add assets and initial version of main cmp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router'; // CLI imports router
+import { ListeHotelsComponent } from './liste-hotels/liste-hotels.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'liste-hotels', component: ListeHotelsComponent },
+]; // sets up routes constant where you define your routes
 
+// configures NgModule imports and exports
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/liste-hotels/liste-hotels.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-hotels',
+  templateUrl: './liste-hotels.component.html',
+  styleUrls: ['./liste-hotels.component.css'],
+})
+export class ListeHotelsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
